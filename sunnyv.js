bd3821effd735fae8087c5e802cc4409cb57090b
smmersch26 <smmersch26@gmail.com>
2017-01-10 00:00:52 -0500

    animate mobile fix 2nd attempt

--- sunnyv.js
+++ sunnyv.js
@@ -20,7 +20,30 @@ function onScrollInit( items, trigger ) {
         });
   });
 }
-if ($(window).width() < 767){
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+
+if (isMobile.any()){
 	$("#tunes").removeClass("os-animation");
 	$("#shows").removeClass("os-animation");
 	$("#videos").removeClass("os-animation");
